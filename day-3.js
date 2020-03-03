d4ffa6155673643d9ffa25299703e3cb6eb66cf9
christiansallen <christiansallen@gmail.com>
2020-03-03 15:29:21 -0700

    added more to day-3

--- day-3.js
+++ day-3.js
@@ -8,3 +8,35 @@ var findNumbers = function(nums) {
   }
   return count;
 };
+
+var maxNumberOfBalloons = function(text) {
+  let b = 0,
+    a = 0,
+    l = 0,
+    o = 0,
+    n = 0,
+    count = 0;
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === "b") {
+      b++;
+    } else if (text[i] === "a") {
+      a++;
+    } else if (text[i] === "l") {
+      l++;
+    } else if (text[i] === "o") {
+      o++;
+    } else if (text[i] === "n") {
+      n++;
+    }
+  }
+  console.log(b, a, l, o, n);
+  while (b >= 1 && a >= 1 && l >= 2 && o >= 2 && n >= 1) {
+    count++;
+    b -= 1;
+    a -= 1;
+    l -= 2;
+    o -= 2;
+    n -= 1;
+  }
+  return count;
+};
